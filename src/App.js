5b32b7b0dfbda1a9ae63908f117a677f2e54f7ed
Wilson <wilsonrueda2@gmail.com>
2020-08-25 22:16:19 -0500

    se crearon las carpetas assetes components and styles para trabajo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './styles/App.css';
+/**Esta funcion tambien se puede cambiar
+ * por una clase que extienda de components
+ */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hola mundo</p>
     </div>
   );
 }
 
 export default App;
+/*lo que esta dentro de la funcion App se retorna en html 
+lo que esta dentro del div que es una etiqueta padre*/
+
+/** */
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,7 +10,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+/**Este archivo es que hace todas las importaciones se deja igual
+ * 
+ */
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
